100eb351300490f12c930de468a856e08079a847
rRahul0 <karmakarrahul503@gmail.com>
2025-07-08 11:09:19 +0530

    Golang Deleted

--- client/app/editor/page.tsx
+++ client/app/editor/page.tsx
@@ -60,11 +60,11 @@ import "fmt"
 func main() {
     fmt.Println("This is made by Rahul Karmakar")
 }`,
-  //   rust: `// Made by Rahul Karmakar
-  // // https://www.linkedin.com/in/rahul-karmakar-605509257/
-  // fn main() {
-  //     println!("This is made by Rahul Karmakar");
-  // }`,
+  rust: `// Made by Rahul Karmakar
+  // https://www.linkedin.com/in/rahul-karmakar-605509257/
+  fn main() {
+      println!("This is made by Rahul Karmakar");
+  }`,
 }
 
 const languages = [
@@ -74,7 +74,7 @@ const languages = [
   { value: "java", label: "Java" },
   { value: "cpp", label: "C++" },
   { value: "c", label: "C" },
-  { value: "go", label: "Go" },
+  // { value: "go", label: "Go" },
   // { value: "rust", label: "Rust" },
 ]
 
